291c3f4da418a23bcef7add4addff9e700e05665
JONGMIN SHIN <jongmin373@gmail.com>
2023-02-22 22:57:44 +0900

    Update user table

--- src/main/java/com/example/career/domain/user/Entity/User.java
+++ src/main/java/com/example/career/domain/user/Entity/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
 
 
 @Data
@@ -17,9 +20,46 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "VARCHAR(30) NOT NULL")
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
     private String name;
 
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String username;
 
+    @Column(columnDefinition = "VARCHAR(30)", unique = true, nullable = false)
+    private String nickname;
+
+    @Column(columnDefinition = "CHAR(13)")
+    private String telephone;
+
+    @Column(nullable = false)
+    private int authType;
+
+    @Column(columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String password;
+
+    @Column(columnDefinition = "CHAR(7)", nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private int status = 0;
+
+    @Column(nullable = false)
+    private Boolean gender = true;
+
+    @Lob
+    private String profile_img;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String introduce;
+
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    @PrePersist // 데이터 생성이 이루어질때 사전 작업
+    public void prePersist() {
+        this.createAt = LocalDateTime.now();
+        this.updateAt = this.createAt;
+    }
 
 }
