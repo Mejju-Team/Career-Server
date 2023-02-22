82d19a70942c5deb84f58a21c4535831de00d176
JONGMIN SHIN <jongmin373@gmail.com>
2023-02-22 22:26:25 +0900

    Create User table

---
+++ src/main/java/com/example/career/domain/user/Entity/User.java
@@ -0,0 +1,25 @@
+package com.example.career.domain.user.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(30) NOT NULL")
+    private String name;
+
+
+
+}
