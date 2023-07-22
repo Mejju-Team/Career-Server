d9d552ddf7498773a04e113968702c41b0f2c452
JONGMIN SHIN <jongmin373@gmail.com>
2023-07-22 15:01:10 +0900

    Test : add Test code at HomeController.java

--- src/main/java/com/example/career/Controller/HomeController.java
+++ src/main/java/com/example/career/Controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.career.Controller;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,13 @@ import java.security.GeneralSecurityException;
 @RequiredArgsConstructor
 
 public class HomeController {
+    @GetMapping("test")
+    public TestDTO conntectTest() {
+        return new TestDTO();
+    }
 
+}
+@Data
+class TestDTO {
+    private int code = 400;
 }
