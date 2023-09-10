1766277b566325e651f0841ded68aedab2f41037
JaejunHan <hanjj03@naver.com>
2023-09-11 01:06:24 +0900

    FEAT: RestControllerAdvice(Exception handler)

--- src/main/java/com/example/career/global/Exception/RestAdvice.java
+++ src/main/java/com/example/career/global/Exception/RestAdvice.java
@@ -1,2 +1,17 @@
-package com.example.career.global.Exception;public class RestAdvice {
+package com.example.career.global.Exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice("com.example.career.domain.community.Controller")
+public class RestAdvice {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> ExceptionHandler(Exception e) {
+        log.info("Exception msg = {}", e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
