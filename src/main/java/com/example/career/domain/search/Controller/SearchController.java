fb00744e009a58bc70dc8fe145c521b4933dc44f
JONGMIN SHIN <jongmin373@gmail.com>
2023-09-14 20:55:54 +0900

    REFECTOR : change return type

--- src/main/java/com/example/career/domain/search/Controller/SearchController.java
+++ src/main/java/com/example/career/domain/search/Controller/SearchController.java
@@ -5,7 +5,10 @@ import com.example.career.domain.community.Repository.ArticleRepository;
 import com.example.career.domain.community.Repository.CommentRepository;
 import com.example.career.domain.search.Dto.CommunitySearchRespDto;
 import com.example.career.domain.search.Service.SearchService;
+import com.example.career.global.valid.ValidCheck;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,10 +17,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("search")
 public class SearchController {
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
     private final SearchService searchService;
 
     @GetMapping("community")
-    public List<CommunitySearchRespDto> searchCommunityList(@RequestParam String keyWord) {
-        return searchService.getArticlesByKeyWord(keyWord);
+    public ResponseEntity<List<CommunitySearchRespDto>> searchCommunityList(@RequestParam String keyWord) {
+        return new ResponseEntity<>(searchService.getArticlesByKeyWord(keyWord), HttpStatus.OK);
   }
 }
