3e61059497cc157368d63c6a13fcfc71417edbdd
JONGMIN SHIN <92903481+JongDo737@users.noreply.github.com>
2024-01-25 10:15:32 +0900

    Modify : update UserBriefWithRate (#122)
    멘토의 상담가능 전공, 소개글 등을 추가했습니다.

--- src/main/java/com/example/career/domain/community/Dto/Brief/UserBriefWithRate.java
+++ src/main/java/com/example/career/domain/community/Dto/Brief/UserBriefWithRate.java
@@ -2,6 +2,7 @@ package com.example.career.domain.community.Dto.Brief;
 
 import com.example.career.domain.user.Entity.TutorDetail;
 import com.example.career.domain.user.Entity.User;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,15 +15,30 @@ public class UserBriefWithRate {
     private String nickname;
     private Boolean isTutor;
     private String profileImg;
+    private String introduce;
     private int rateCount;
     private float rateAvg;
+    private String birth;
+    private String consultMajor1;
+
+    private String consultMajor2;
+
+    private String consultMajor3;
+    private String consultMethod;
 
     public UserBriefWithRate(User user, TutorDetail tutorDetail) {
         this.id = user.getId();
         this.nickname = user.getNickname();
         this.isTutor = user.getIsTutor();
         this.profileImg = user.getProfileImg();
+        this.birth = user.getBirth();
+        this.introduce = user.getIntroduce();
+
         this.rateCount = tutorDetail.getRateCount();
         this.rateAvg = tutorDetail.getRateAvg();
+        this.consultMajor1 = tutorDetail.getConsultMajor1();
+        this.consultMajor2 = tutorDetail.getConsultMajor2();
+        this.consultMajor3 = tutorDetail.getConsultMajor3();
+        this.consultMethod = tutorDetail.getConsultMethod();
     }
 }
